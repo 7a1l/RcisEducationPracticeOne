bdc8e33069475575e1d1718a28b49ddede5720c9
Федоров Павел Олегович <gr612_fepol@ttit.local>
2022-10-22 10:38:04 +0700

    First commit. Create console app

---
+++ rcis1/bin/rcis1.dart
@@ -0,0 +1,5 @@
+import 'package:rcis1/rcis1.dart' as rcis1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rcis1.calculate()}!');
+}
---
+++ rcis1/test/rcis1_test.dart
@@ -0,0 +1,8 @@
+import 'package:rcis1/rcis1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
